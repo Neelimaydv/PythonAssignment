34b5aadccfade5b4c539477556a42aeea6329e11
Neelimaydv <neelimay67@gmail.com>
2026-02-07 18:35:24 +0530

    Assignment 2 Module 3: Control Structures in Python
    ASSIGNMENT 2:
    Module 3: Control Structures in Python
    task1-even and odd
    task2--sum of 50 integer

---
+++ ASSIGNMENT 2-Module3-TASK1-evenodd.py
@@ -0,0 +1,5 @@
+A=int(input('Enter your number:'))
+if A%2==0:
+    print(A,'is even')
+else:
+    print(A,'is odd')
---
+++ ASSIGNMENT 2-Module3-TASK2-Sum50.py
@@ -0,0 +1,4 @@
+sum=0
+for A in range(1,51):
+   sum=sum+A
+print('The sum of 1 to 50 is ',sum)
